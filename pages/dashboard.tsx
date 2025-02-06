45278bf2939eaa78b34fbb4d3e0092883cdca050
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2025-02-07 02:32:24 +0530

    Refactor dashboard component to improve session handling and user data access

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,39 +2,45 @@ import Chat from '@/components/Chat';
 import FriendList from '@/components/FriendList';
 import FriendRequest from '@/components/FriendRequest';
 import HandleRequests from '@/components/HandleRequests';
-import { signOut, useSession } from 'next-auth/react';
+import { getSession, signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
-  const { data: session } = useSession();
-  // const router = useRouter();
+  const session = useSession();
+  const router = useRouter();
   const [selectedFriendId, setSelectedFriendId] = useState<string | null>(null);
 
   useEffect(() => {
     if (!session) {
-      return;
+      router.push('/login');
     }
-  }, []);
+  }, [session]);
 
+  if (!session) {
+    return <div>Not logged in</div>;
+  }
 
+  console.log(session.data);
 
   return (
     <>
-      {session && (
+      {session.data && (
         <div className="p-6">
-          <h1 className="text-2xl">Welcome, {session?.user?.email || ''}!</h1>
+          <h1 className="text-2xl">
+            Welcome, {session?.data?.user?.email || ''}!
+          </h1>
           <p>You are now logged in.</p>
           <button onClick={() => signOut({ callbackUrl: '/' })}>Logout</button>
-          <FriendRequest userId={session?.user?.id || ''} />
-          <HandleRequests userId={session?.user?.id || ''} />
+          <FriendRequest userId={session?.data?.user?.id || ''} />
+          <HandleRequests userId={session?.data?.user?.id || ''} />
           <FriendList
-            userId={session?.user?.id || ''}
+            userId={session?.data?.user?.id || ''}
             onSelectFriend={setSelectedFriendId}
           />
           {selectedFriendId ? (
             <Chat
-              userId={session?.user?.id || ''}
+              userId={session?.data?.user?.id || ''}
               friendId={selectedFriendId}
             />
           ) : (
