644016c315f80a662243b5ca061f05c3b87d410d
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2025-02-07 14:47:35 +0530

    Update WebSocket connection URL to support production environment

--- src/data/utils/socket.ts
+++ src/data/utils/socket.ts
@@ -1,11 +1,14 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://chat-app-front-rose.vercel.app/'
+    : 'http://localhost:3000/'; // Local WebSocket server
 let socket: Socket | null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    socket = io({ path: '/api/socket' });
+    socket = io(SOCKET_URL, { path: '/api/socket' });
   }
   return socket;
 };
