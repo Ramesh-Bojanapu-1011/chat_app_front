c8865f1a486b37bca984dd36d7a631d3bf0db938
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2025-02-07 15:41:47 +0530

    Update socket initialization to use dynamic SITE_URL for improved flexibility

--- src/data/utils/socket.ts
+++ src/data/utils/socket.ts
@@ -1,14 +1,10 @@
 import { io, Socket } from 'socket.io-client';
 
-// const SOCKET_URL =
-//   process.env.NODE_ENV === 'production'
-//     ? 'https://chat-app-front-rose.vercel.app/'
-//     : 'http://localhost:3000/'; // Local WebSocket server
 let socket: Socket | null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    socket = io({ path: '/api/socket' });
+    socket = io(process.env.SITE_URL, { path: '/api/socket' });
   }
   return socket;
 };
