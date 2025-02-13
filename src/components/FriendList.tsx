9adae00b3ccdf201fd79c7405d4dfd1fc9b4eaed
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2025-02-13 21:40:49 +0530

    Comment out unread count display in FriendList component

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -77,7 +77,7 @@ server and updating the state with that count. Here's a breakdown of what it doe
         <h2 className="text-lg font-semibold">Friends List</h2>
         {friends.length === 0 ? <p>No friends found</p> : null}
         {friends.map((friend) => {
-          const friendId = friend._id.toString();
+          
           return (
             <button
               key={friend._id}
@@ -88,9 +88,9 @@ server and updating the state with that count. Here's a breakdown of what it doe
                 className={`w-3 h-3 p-4 rounded-full ${friend.isOnline ? 'bg-green-500' : 'bg-gray-400'}`}
               >
                 {friend.username}
-                {unreadCounts[friendId] > 0 && (
+                {/* {unreadCount > 0 && (
                   <span className="text-xl">{unreadCounts[friendId]}</span>
-                )}
+                )} */}
 
                 <span className="text-gray-600 text-sm">
                   {friend.isOnline
