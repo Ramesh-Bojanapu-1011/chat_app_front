d78fd3b252d49861edf08a2d81b45757f088a6ed
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2025-02-18 10:43:06 +0530

    remove facebook login

--- src/data/login/loginoption.ts
+++ src/data/login/loginoption.ts
@@ -1,27 +1,3 @@
-// {
-//     google: {
-//       id: 'google',
-//       name: 'Google',
-//       type: 'oauth',
-//       signinUrl: 'http://localhost:3000/api/auth/signin/google',
-//       callbackUrl: 'http://localhost:3000/api/auth/callback/google'
-//     },
-//     facebook: {
-//       id: 'facebook',
-//       name: 'Facebook',
-//       type: 'oauth',
-//       signinUrl: 'http://localhost:3000/api/auth/signin/facebook',
-//       callbackUrl: 'http://localhost:3000/api/auth/callback/facebook'
-//     },
-//     credentials: {
-//       id: 'credentials',
-//       name: 'Credentials',
-//       type: 'credentials',
-//       signinUrl: 'http://localhost:3000/api/auth/signin/credentials',
-//       callbackUrl: 'http://localhost:3000/api/auth/callback/credentials'
-//     }
-//   }
-
 export const loginOptions = {
   google: {
     id: 'google',
@@ -29,13 +5,6 @@ export const loginOptions = {
     type: 'oauth',
     signinUrl: `${process.env.NEXTAUTH_URL}/api/auth/signin/google`,
     callbackUrl: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`,
-  },
-  facebook: {
-    id: 'facebook',
-    name: 'Facebook',
-    type: 'oauth',
-    signinUrl: `${process.env.NEXTAUTH_URL}/api/auth/signin/facebook`,
-    callbackUrl: `${process.env.NEXTAUTH_URL}/api/auth/callback/facebook`,
   },
   credentials: {
     id: 'credentials',
